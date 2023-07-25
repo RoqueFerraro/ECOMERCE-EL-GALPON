ff47630789a38f4d434578c15b2ec042fab40c1c
Roque Ferraro <lroquegferraro@gmail.com>
2023-07-25 20:45:08 -0300

    Pre-entrega 3

---
+++ JS/productos.js
@@ -0,0 +1,24 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "proteina",
+      precio: 5000,
+      img: 'img/ENA 1.webp',
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Creatina",
+      precio: 10000,
+      img:'img/ENA 2.webp',
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Mass gainer",
+      precio: 15000,
+      img:'img/ENA 3.webp',
+      cantidad: 1,
+    },
+    
+  ];
